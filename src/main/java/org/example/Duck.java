bd46366de5ad8f32a68fb34018bcde19226effeb
Srijala Poudel <srijana.poudel@tuni.fi>
2023-11-24 20:34:53 +0200

    Duck and Turkey classes is added

--- src/main/java/org/example/Duck.java
+++ src/main/java/org/example/Duck.java
@@ -1,5 +1,7 @@
 package org.example;
 
 public class Duck {
-
+    public void quack() {
+        System.out.println("Duck quacking");
+    }
 }
--- src/main/java/org/example/Turkey.java
+++ src/main/java/org/example/Turkey.java
@@ -1,4 +1,10 @@
 package org.example;
 
 public class Turkey {
+    public void gobble() {
+    }
+
+    public void flyShortDistance() {
+
+    }
 }
